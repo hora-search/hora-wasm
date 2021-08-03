1cba6772e9afee727e85ee591852a194ff5567be
Mongkii <mongkii@hotmail.com>
2021-08-04 03:12:37 +0800

    chore: edited package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 const dist = path.resolve(__dirname, "dist");
