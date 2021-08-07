f806ab6ea53dcc1e3afc994d258ce306cd5902a4
aljun <salamer_gaga@163.com>
2021-08-07 14:15:59 +0800

    add: add no_thread feature (#3)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
 import("../pkg/index.js").catch(console.error);
-
-// import init, { initThreadPool /* ... */ } from './pkg/index.js';
-
-// await init();
-
-// await initThreadPool(navigator.hardwareConcurrency);
